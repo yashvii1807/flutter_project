fc317e9a9d7ecff1e35defc7c00fb62fd6df73dd
yashvii1807 <sidaparayashvi18@gmail.com>
2025-07-29 08:50:28 +0530

    stopwatch

---
+++ lib/StopwatchExperiemnt.dart
@@ -0,0 +1,94 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class StopwatchExperiemnt extends StatefulWidget {
+  const StopwatchExperiemnt({super.key});
+
+  @override
+  State<StopwatchExperiemnt> createState() => _StopwatchExperiemntState();
+}
+
+class _StopwatchExperiemntState extends State<StopwatchExperiemnt> {
+  double seconds = 0;
+  late Timer timer;
+  bool isTicking = false;
+  int millis = 0;
+  String _secondtoText() => seconds <= 1 ? 'Second' : 'Seconds';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Stopwatch Experiment')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              '$seconds ${_secondtoText()}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: isTicking ? null : _starttimer,
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.green),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                ),
+                child: const Text("Start"),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: isTicking ? _stoptimer : null,
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.red),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                ),
+                child: const Text("Stop"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _starttimer() {
+    timer = Timer.periodic(const Duration(milliseconds: 100), _onTick);
+    setState(() {
+      isTicking = true;
+      millis = 0;
+    });
+  }
+
+  void _stoptimer() {
+    timer.cancel();
+    setState(() {
+      isTicking = false;
+    });
+  }
+
+  void _onTick(Timer timer) {
+    if (mounted) {
+      setState(() {
+        millis += 100;
+        seconds = millis / 1000;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+}
